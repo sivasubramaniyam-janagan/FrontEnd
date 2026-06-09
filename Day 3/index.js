394f36bfc0d97f9ca4894dc995e89b5fd603152e
sivasubramaniyam-janagan <sivasubramaniyamjanagan@gmail.com>
2026-06-09 07:00:14 +0530

    Update index.js

--- Day 3/index.js
+++ Day 3/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 import Student from './models/student.js'
 import studentRouter from  './routers/studentRouter.js'
 
-const mongoURI = "mongodb://admin:GzqUacDa6qP0j0dM@ac-yzspfdr-shard-00-00.2mlu19f.mongodb.net:27017,ac-yzspfdr-shard-00-01.2mlu19f.mongodb.net:27017,ac-yzspfdr-shard-00-02.2mlu19f.mongodb.net:27017/?ssl=true&replicaSet=atlas-xj0ilt-shard-0&authSource=admin&appName=Cluster0"
+const mongoURI = "mon"
 const app = express()
 
 mongoose.connect(mongoURI).then(
