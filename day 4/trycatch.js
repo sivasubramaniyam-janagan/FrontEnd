061239ea39deb1aca1ab1c80a4a83e042c6abb1a
sivasubramaniyam-janagan <sivasubramaniyamjanagan@gmail.com>
2026-06-10 10:12:51 +0530

    day 4 completed

---
+++ day 4/trycatch.js
@@ -0,0 +1,16 @@
+console.log("system started")
+
+let student={
+    "name":"jana",
+    "age":22
+}
+
+try{
+    console.log( student.name)
+    console.log( student.parent.name) 
+}
+catch(err){
+    console.log("err")
+}
+
+console.log("good night")
