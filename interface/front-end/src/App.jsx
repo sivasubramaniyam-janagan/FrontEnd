26f445d5553b3df5ed929467ee004e97efab7f89
sivasubramaniyam-janagan <sivasubramaniyamjanagan@gmail.com>
2026-06-16 06:23:42 +0530

    minor improvements

---
+++ interface/front-end/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import './components/productCard.jsx'
+import ProductCard from './components/productCard.jsx'
+
+function App() {
+  return (
+    <>
+      <div>
+        <ProductCard/>
+        <ProductCard/>
+      </div> 
+    </>
+  )
+}
+
+export default App
---
+++ interface/front-end/src/components/productCard.jsx
@@ -0,0 +1,9 @@
+export default function ProductCard(){
+    return (
+            <div className="bg-black ">
+                <h1 className="text-white">Hello janagan</h1>
+                <p>5000</p>
+                <button>buynow</button>
+            </div>
+    )
+} 
